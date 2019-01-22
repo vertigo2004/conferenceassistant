f170c438c67e1835163e53113d1a93714d34ec4b
Andrii Kukh <AkukhA@gmail.com>
2019-01-22 19:56:08 +0200

    added @Entity Conference

---
+++ src/main/java/domain/Conference.java
@@ -0,0 +1,52 @@
+package domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Conference {
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    private Long id;
+    private String name;
+    private String info;
+    //private List<Topic> topics;
+
+    public Conference() {
+    }
+    public Conference(String name,
+                      String info
+                      //,List<Topic> topics
+    ) {
+        this.name = name;
+        this.info = info;
+        //this.topics = topics;
+    }
+
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getInfo() {
+        return info;
+    }
+    public void setInfo(String info) {
+        this.info = info;
+    }
+    /*public List<Topic> getTopics() {
+        return topics;
+    }*/
+    /*public void setTopics(List<Topic> topics) {
+        this.topics = topics;
+    }*/
+}
