9b2fc2bf55ce12f2dc2d2584e26dfbf425320c70
amfibiotik <biktop@bigmir.net>
2019-01-23 17:16:56 +0200

    Add "Question" entity Add "State" enum

---
+++ src/main/java/ifit/cluster/cassistant/domain/Question.java
@@ -0,0 +1,36 @@
+package ifit.cluster.cassistant.domain;
+
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Question {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long Id;
+
+    private String email;
+    private String text;
+    private int rate;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    private Topic topic;
+
+    @ElementCollection(targetClass = State.class, fetch = FetchType.EAGER)
+    @CollectionTable(name = "user_role", joinColumns = @JoinColumn(name = "user_id"))
+    @Enumerated(EnumType.STRING)
+    private Set<State> state;
+
+
+}
---
+++ src/main/java/ifit/cluster/cassistant/domain/State.java
@@ -0,0 +1,5 @@
+package ifit.cluster.cassistant.domain;
+
+public enum State {
+    NEW, IN_PROGRESS, ANSWERED, REMOVED
+}
