fe4d646ae2e8c9eb5082852f2c73bf28ac3fa856
Oleg Pashkevych <vertigo.if@gmail.com>
2019-01-28 21:05:02 +0200

    Thymeleaf початок

--- src/main/java/ifit/cluster/cassistant/controller/ConferenceController.java
+++ src/main/java/ifit/cluster/cassistant/controller/ConferenceController.java
@@ -3,26 +3,33 @@ package ifit.cluster.cassistant.controller;
 import ifit.cluster.cassistant.domain.Conference;
 import ifit.cluster.cassistant.repo.ConferenceRepo;
 import ifit.cluster.cassistant.repo.TopicRepo;
+import ifit.cluster.cassistant.service.ConferenceService;
 import ifit.cluster.cassistant.service.TopicService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class ConferenceController {
     @Autowired
-    private ConferenceRepo conferenceRepo;
-    @Autowired
-    private TopicRepo topicRepo;
-    @Autowired
-    private TopicService topicService;
+    private ConferenceService conferenceService;
 
     @GetMapping("/")
     public String conference(Model model){
-        Conference conference = conferenceRepo.getById(1L);
+        Conference conference = conferenceService.getById(1L);
         model.addAttribute("conference", conference);
 
         return "Conference";
     }
+
+    @GetMapping("/conf/{id}")
+    public String conference(@PathVariable Long id, Model model){
+        Conference conference = conferenceService.getById(id);
+        model.addAttribute("conference", conference);
+
+        return "conference";
+    }
+
 }
