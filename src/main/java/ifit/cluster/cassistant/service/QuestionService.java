df3d6e516d939f20a2b879877290d1a133fd7812
amfibiotik <biktop@bigmir.net>
2019-01-30 00:31:48 +0200

    Fix some code style

--- src/main/java/ifit/cluster/cassistant/service/QuestionService.java
+++ src/main/java/ifit/cluster/cassistant/service/QuestionService.java
@@ -1,6 +1,5 @@
 package ifit.cluster.cassistant.service;
 
-import com.sun.org.apache.bcel.internal.generic.NEW;
 import ifit.cluster.cassistant.domain.Question;
 import ifit.cluster.cassistant.domain.State;
 import ifit.cluster.cassistant.repo.QuestionRepo;
@@ -9,7 +8,6 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class QuestionService {
-
     @Autowired
     private QuestionRepo questionRepo;
 
@@ -19,9 +17,9 @@ public class QuestionService {
 
     boolean checkEmail(String email) {
         return questionRepo.checkEmail(email);
-    };
+    }
 
     void updateState(Question question, State state) {
         question.setState(state);
-    };
+    }
 }
